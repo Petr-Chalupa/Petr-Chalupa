a2d75fadbbdcde53ff918688951d7c427d159dfa
Petr Chalupa <58788289+Petr-Chalupa@users.noreply.github.com>
2023-02-07 17:22:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.querySelector(".btn_cv_download").addEventListener("click", () => {
     const link = document.createElement("a");
     link.href = "./PetrChalupa_CV.pdf";
-    link.download = true;
+    link.download = "PetrChalupa_CV.pdf";
     link.click();
 });
