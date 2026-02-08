1d84566d7f228c32019ac9c4a7e3a52cfb3e4bfc
Petr Chalupa <58788289+Petr-Chalupa@users.noreply.github.com>
2026-02-08 12:15:58 +0100

    Data driven web

---
+++ web/.eleventy.js
@@ -0,0 +1,16 @@
+import yaml from "js-yaml";
+
+export default function (eleventyConfig) {
+    eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+
+    eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addPassthroughCopy("main.css");
+
+    return {
+        dir: {
+            input: ".",
+            data: "../",
+            output: "../dist",
+        },
+    };
+}
