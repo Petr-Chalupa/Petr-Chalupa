5eb58c9f9b762087bffe63be016cb94728097770
Petr Chalupa <58788289+Petr-Chalupa@users.noreply.github.com>
2026-02-08 13:49:11 +0100

    Link

--- web/.eleventy.js
+++ web/.eleventy.js
@@ -5,6 +5,16 @@ export default function (eleventyConfig) {
     const data = yaml.load(fs.readFileSync("../data.yaml", "utf8"));
     eleventyConfig.addGlobalData("data", data);
 
+    eleventyConfig.addFilter("domain", function (url) {
+        try {
+            if (!url) return "";
+            const urlObj = new URL(url);
+            return urlObj.hostname.replace(/^www\./, "");
+        } catch (e) {
+            return url;
+        }
+    });
+
     eleventyConfig.addPassthroughCopy("img");
     eleventyConfig.addPassthroughCopy("main.css");
 
