dc0531abf6d5c721cd6bfb1a45f54079862a2244
Petr Chalupa <58788289+Petr-Chalupa@users.noreply.github.com>
2026-02-08 13:31:14 +0100

    cc

--- web/.eleventy.js
+++ web/.eleventy.js
@@ -6,9 +6,6 @@ export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("img");
     eleventyConfig.addPassthroughCopy("main.css");
 
-    eleventyConfig.ignores.add("node_modules");
-    eleventyConfig.ignores.add("../web/node_modules");
-
     return {
         dir: {
             input: ".",
